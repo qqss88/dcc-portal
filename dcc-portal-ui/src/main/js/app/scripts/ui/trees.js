bed107f5f666570850c0720caadd016598b78aa1
mwdchang <mwdchang@gmail.com>
2014-12-09 23:35:06 -0500

    (portal-ui) add first round of feed back for inferred trees, still needs to resolve unknown relations for display

--- dcc-portal-ui/src/main/js/app/scripts/ui/trees.js
+++ dcc-portal-ui/src/main/js/app/scripts/ui/trees.js
@@ -57,43 +57,53 @@ angular.module('icgc.ui.trees', []).directive('pathwayTree', function($compile) 
     template: '<div class="tree"></div>',
     link: function($scope, $element) {
 
-      function getRelation(relation) {
+      // If grand child exist, this is inferred.
+      function getRelation(relation, hasGrandChild) {
         var element = angular.element('<abbr>').attr('data-tooltip-placement', 'left');
 
         if (relation === 'is_a') {
-          return element.text('I ');
+          return element.text('I ')
+            .attr('data-tooltip', hasGrandChild? 'Inferred is a' : 'Is a')
+            .attr('class', 'goterm_is_a');
         } else if (relation === 'part_of') {
           return element.text('P')
-            .attr('data-tooltip', 'Inferred part of')
+            .attr('data-tooltip', hasGrandChild? 'Inferred part of' : 'Part of')
             .attr('class', 'goterm_part_of');
         } else if (relation === 'regulates') {
           return element.text('R')
-            .attr('data-tooltip', 'Inferred regulates')
+            .attr('data-tooltip', hasGrandChild? 'Inferred regulates' : 'Regulates')
             .attr('class', 'goterm_regulates');
         } else if (relation === 'positively_regulates') {
           return element.text('R')
-            .attr('data-tooltip', 'Inferred positively regulates')
+            .attr('data-tooltip', hasGrandChild? 'Inferred positively regulates' : 'Positively regulates')
             .attr('class', 'goterm_regulates');
         } else if (relation === 'negatively_regulates') {
           return element.text('R')
-            .attr('data-tooltip', 'Inferred negatively regulates')
+            .attr('data-tooltip', hasGrandChild? 'Inferred negatively regulates' : 'Negatively regulates')
             .attr('class', 'goterm_regulates');
+        } else {
+          // FIXME: need to find out how to resolve unknowns
+          return element.text('?');
         }
-        return null;
+        // return null;
       }
 
       function addNesting(e, current) {
-        var ul, li, span, anchor, relation, label;
+        var ul, li, span, anchor, relation, label, hasGrandChild = false;
 
         ul = angular.element('<ul>');
         li = angular.element('<li>');
         span = angular.element('<span>');
 
 
+        if (current.child && current.child.child) {
+          hasGrandChild = true;
+        }
+
         current.goTerms.forEach(function(goTerm) {
           label = ' ' + goTerm.id + ' ' + goTerm.name;
           anchor = angular.element('<a>').attr('data-ng-href', '/genesets/' + goTerm.id).text(label);
-          relation = getRelation(goTerm.relation);
+          relation = getRelation(goTerm.relation, hasGrandChild);
 
           if (relation) {
             relation.appendTo(span);
