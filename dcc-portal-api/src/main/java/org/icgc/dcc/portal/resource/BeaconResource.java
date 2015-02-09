a476a903afa719f7da8e1d750f181b5ea32466e0
alyh <aly.hassan9@gmail.com>
2015-02-09 16:40:08 -0500

    Fixed a typo in Beacon Resource and made constants static

--- dcc-portal-api/src/main/java/org/icgc/dcc/portal/resource/BeaconResource.java
+++ dcc-portal-api/src/main/java/org/icgc/dcc/portal/resource/BeaconResource.java
@@ -55,21 +55,21 @@ import com.yammer.metrics.annotation.Timed;
 public class BeaconResource extends BaseResource {
 
   private final BeaconService beaconService;
-  private final Map<String, Integer> CHROMOSOME_LENGTHS =
+  private static final Map<String, Integer> CHROMOSOME_LENGTHS =
       new ImmutableMap.Builder<String, Integer>()
           .put("1", 249250621).put("2", 243199373).put("3", 198022430).put("4", 191154276).put("5", 180915260)
-          .put("6", 171115067).put("7", 159138663).put("8", 146364022).put("9.", 141213431).put("10", 135534747)
+          .put("6", 171115067).put("7", 159138663).put("8", 146364022).put("9", 141213431).put("10", 135534747)
           .put("11", 135006516).put("12", 133851895).put("13", 115169878).put("14", 107349540).put("15", 102531392)
           .put("16", 90354753).put("17", 81195210).put("18", 78077248).put("19", 59128983).put("20", 63025520)
           .put("21", 48129895).put("22", 51304566).put("X", 155270560).put("Y", 59373566).put("MT", 16569).build();
 
-  private final Pattern ALLELE_REGEX = Pattern.compile("^[ACTG]+");
-  private final String WILDCARD_ANY = "ANY";
-  private final String WILDCARD_DEL = "D";
-  private final String WILDCARD_INS = "I";
-  private final String CHROMOSOME_X = "X";
-  private final String CHROMOSOME_Y = "Y";
-  private final String CHROMOSOME_MT = "MT";
+  private static final Pattern ALLELE_REGEX = Pattern.compile("^[ACTG]+");
+  private static final String WILDCARD_ANY = "ANY";
+  private static final String WILDCARD_DEL = "D";
+  private static final String WILDCARD_INS = "I";
+  private static final String CHROMOSOME_X = "X";
+  private static final String CHROMOSOME_Y = "Y";
+  private static final String CHROMOSOME_MT = "MT";
 
   @GET
   @ApiOperation(value = "Beacon", nickname = "Beacon", response = Beacon.class,
