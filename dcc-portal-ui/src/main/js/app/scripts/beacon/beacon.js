c4cf590454b6b4ff6fb921d21c1a50dc7dac4b57
alyh <aly.hassan9@gmail.com>
2015-02-09 16:43:58 -0500

    Added functionality to Beacon page and made fixes

--- dcc-portal-ui/src/main/js/app/scripts/beacon/beacon.js
+++ dcc-portal-ui/src/main/js/app/scripts/beacon/beacon.js
@@ -5,7 +5,7 @@
  * License v3.0. You should have received a copy of the GNU General Public License along with this
  * program. If not, see <http://www.gnu.org/licenses/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
@@ -39,10 +39,102 @@
 
   var module = angular.module('icgc.beacon.controllers', []);
 
-  module.controller('BeaconCtrl', function ($scope, Page) {
+  module.controller('BeaconCtrl', function ($scope, Page, Restangular) {
     Page.setTitle('Beacon');
     Page.setPage('beacon');
 
+    var projectsPromise = Restangular.one('projects')
+      .get({
+        'field' : 'id',
+        'size' : 100
+      },{'Accept':'application/json'});
+
+    projectsPromise.then(function(data){
+      $scope.projects = data.hits;
+      $scope.projects.push({id:'ALL'});
+      $scope.params = {
+        project:$scope.projects[$scope.projects.length-1],
+        chr:'1',
+        reference:'GRCh37',
+      };
+    });
+
+    $scope.hasInvalidParams = true;
+    $scope.result = {
+      exists:false,
+      value:''
+    };
+
+    $scope.checkParams = function(){
+      //first if anything required is empty or missing, stop checking
+      if(!($scope.params.reference && $scope.params.position)){
+        $scope.hasInvalidParams = true;
+        return;
+      }
+
+      $scope.hasInvalidParams = false;
+    };
+
+    $scope.errorMessage='error error';
+    $scope.submitQuery = function() {
+      var promise = Restangular.one('beacon', 'query')
+      .get({
+        'chromosome' : $scope.params.chr,
+        'reference':$scope.params.reference,
+        'position':$scope.params.position.replace(/^0+/,''),
+        'allele':$scope.params.allele ? $scope.params.allele : ''
+      },{'Accept':'application/json'});
+
+      promise.then(function(data){
+        $scope.result.exists = true;
+        $scope.result.value = data.response.exists;
+        var url = data.getRequestedUrl();
+        $scope.requestedUrl = url.substring(0,url.indexOf('?'))+'/query'+url.substring(url.indexOf('?'));
+      });
+    };
+
+    $scope.resetQuery = function() {
+      $scope.params = {
+        project:$scope.projects[$scope.projects.length-1],
+        chr:'1',
+        reference:'GRCh37',
+      };
+      $scope.hasEmptyParams = true;
+      $scope.result = {
+        exists:false,
+        value:''
+      };
+      $scope.requestedUrl = null;
+    };
+  });
+
+  module.directive('positionValidator', function() {
+    return {
+      require: '?ngModel',
+      link: function(scope, element, attrs, ngModelCtrl) {
+        if(!ngModelCtrl) {
+          return;
+        }
+
+        ngModelCtrl.$parsers.push(function(val) {
+          if (angular.isUndefined(val)) {
+            val = '';
+          }
+          var clean = val.replace( /[^0-9]+/g, '');
+          if (val !== clean) {
+            ngModelCtrl.$setViewValue(clean);
+            ngModelCtrl.$render();
+          }
+          return clean;
+        });
+
+        element.bind('keypress', function(event) {
+          if(event.keyCode === 32) {
+            event.preventDefault();
+          }
+        });
+      }
+    };
   });
 
 })();
