04e121de1ebccf91e7b7f66f673ceaadebbb85c0
alyh <aly.hassan9@gmail.com>
2015-03-10 15:55:29 -0400

    Moved integer validator to new file

--- dcc-portal-ui/src/main/js/app/scripts/beacon/beacon.js
+++ dcc-portal-ui/src/main/js/app/scripts/beacon/beacon.js
@@ -230,30 +230,6 @@ var DATASET_ALL = 'All Projects';
     });
   });
 
-  module.directive('positionValidator', function() {
-    return {
-      require: '?ngModel',
-      link: function(scope, element, attrs, ngModelCtrl) {
-        if(!ngModelCtrl) {
-          return;
-        }
-
-        ngModelCtrl.$parsers.push(function(val) {
-          if (angular.isUndefined(val)) {
-            val = '';
-          }
-          var clean = val.replace( /[^0-9]+/g, '');
-          clean = clean.replace(/^0+/,'');
-          if (val !== clean) {
-            ngModelCtrl.$setViewValue(clean);
-            ngModelCtrl.$render();
-          }
-          return clean;
-        });
-      }
-    };
-  });
-
   module.directive('alleleValidator', function() {
     return {
       require: '?ngModel',
