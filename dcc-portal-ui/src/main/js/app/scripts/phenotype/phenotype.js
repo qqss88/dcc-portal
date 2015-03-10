9ec385b19595be850afae16a154a4d8ff08cd0f5
mwdchang <mwdchang@gmail.com>
2015-03-10 13:29:47 -0400

    (portal-ui) Use thinner profile on form elements to conserve space. Adjust modal and upload gene set dialog to be more dynamic

--- dcc-portal-ui/src/main/js/app/scripts/phenotype/phenotype.js
+++ dcc-portal-ui/src/main/js/app/scripts/phenotype/phenotype.js
@@ -24,36 +24,36 @@
       link: function($scope) {
         $scope.testChart = {};
         $scope.testChart.series = [
-           {
-              name: 'S1',
-              data: [40, 50, 60]
-           },
-           {
-              name: 'S2',
-              data: [80, 50, 20]
-           },
-           {
-              name: 'S3',
-              data: [10, 90, 20]
-           }
+          {
+            name: 'S1',
+            data: [40, 50, 60]
+          },
+          {
+            name: 'S2',
+            data: [80, 50, 20]
+          },
+          {
+            name: 'S3',
+            data: [10, 90, 20]
+          }
 
         ];
         $scope.testChart.categories = ['Male', 'Female', 'No Data'];
 
         $scope.testChart2 = {};
         $scope.testChart2.series = [
-           {
-              name: 'S1',
-              data: [40, 50, 60, 10, 20, 40, 60, 90]
-           },
-           {
-              name: 'S2',
-              data: [30, 40, 30, 20, 30, 40, 30, 20]
-           },
-           {
-              name: 'S3',
-              data: [10, 20, 40, 40, 80, 10, 10, 10]
-           }
+          {
+            name: 'S1',
+            data: [40, 50, 60, 10, 20, 40, 60, 90]
+          },
+          {
+            name: 'S2',
+            data: [30, 40, 30, 20, 30, 40, 30, 20]
+          },
+          {
+            name: 'S3',
+            data: [10, 20, 40, 40, 80, 10, 10, 10]
+          }
         ];
         $scope.testChart2.categories = ['1-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80'];
 
