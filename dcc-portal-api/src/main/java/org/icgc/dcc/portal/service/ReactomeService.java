693d0e7c268d97c01b0402611f362a8d678bf3e6
alyh <aly.hassan9@gmail.com>
2015-03-26 15:21:59 -0400

    Added new endpoint for reactome

---
+++ dcc-portal-api/src/main/java/org/icgc/dcc/portal/service/ReactomeService.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.portal.service;
+
+import static com.google.common.base.Charsets.UTF_8;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import lombok.val;
+
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Maps;
+import com.google.common.io.LineProcessor;
+import com.google.common.io.Resources;
+
+@Service
+public class ReactomeService {
+
+  private final Client client;
+  private final String index;
+
+  @Autowired
+  public ReactomeService(Client client, @Value("#{indexName}") String index) {
+    this.index = index;
+    this.client = client;
+  }
+
+  public Map<String, String> getProteinIdMap() {
+    val map = Maps.<String, String> newHashMap();
+    try {
+      Resources.readLines(new URL(""), UTF_8, new LineProcessor<Boolean>() {
+
+        @Override
+        public boolean processLine(String line) {
+          String[] values = line.split("\t");
+          return true;
+        }
+
+        @Override
+        public Boolean getResult() {
+          return true;
+        }
+      });
+    } catch (MalformedURLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return map;
+  }
+}
