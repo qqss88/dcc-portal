fb06448e0a360dd7a4a562061da96da3924592a4
mwdchang <mwdchang@gmail.com>
2015-06-10 10:40:08 -0400

    (portal-api) add donor cardinality facets

--- dcc-portal-api/src/main/java/org/icgc/dcc/portal/repository/RepositoryFileRepository.java
+++ dcc-portal-api/src/main/java/org/icgc/dcc/portal/repository/RepositoryFileRepository.java
@@ -24,6 +24,7 @@ import static org.elasticsearch.index.query.FilterBuilders.missingFilter;
 import static org.elasticsearch.index.query.FilterBuilders.nestedFilter;
 import static org.elasticsearch.index.query.FilterBuilders.termsFilter;
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
+import static org.elasticsearch.search.aggregations.AggregationBuilders.cardinality;
 import static org.elasticsearch.search.aggregations.AggregationBuilders.filter;
 import static org.elasticsearch.search.aggregations.AggregationBuilders.global;
 import static org.elasticsearch.search.aggregations.AggregationBuilders.missing;
@@ -73,6 +74,7 @@ import org.elasticsearch.search.aggregations.bucket.missing.Missing;
 import org.elasticsearch.search.aggregations.bucket.nested.Nested;
 import org.elasticsearch.search.aggregations.bucket.nested.ReverseNested;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.metrics.cardinality.Cardinality;
 import org.elasticsearch.search.aggregations.metrics.sum.Sum;
 import org.elasticsearch.search.sort.SortOrder;
 import org.icgc.dcc.portal.model.IndexModel;
@@ -226,12 +228,13 @@ public class RepositoryFileRepository {
             .subAggregation(terms(repoFiltered).size(1024).field(field))
             .subAggregation(missing("_missing").field(field))));
 
-    // Special filtered case - reposizes
+    // Special filtered case - repo sizes and repo donors
     val repoSizeFitered = "repositorySizes";
     aggs.add(global(repoSizeFitered)
         .subAggregation(filter(repoSizeFitered).filter(buildRepoFilters(filters.deepCopy(), false))
             .subAggregation(terms(repoSizeFitered).size(1024).field(field)
-                .subAggregation(sum(repoSizeFitered).field("repository.file_size")))));
+                .subAggregation(cardinality("donor").precisionThreshold(40000).field("donor.donor_id"))
+                .subAggregation(sum("fileSize").field("repository.file_size")))));
 
     // Special nested case - Disabled as currently there are no nested fields
     // for (String facet : FACETS) {
@@ -278,7 +281,8 @@ public class RepositoryFileRepository {
       // result.put(name, convertNormalAggregation(agg));
       // }
       if (name.equals("repositorySizes")) {
-        result.put(name, convertSumAggregation(agg));
+        result.put("repositorySizes", convertRepoSizeAggregation(agg));
+        result.put("repositoryDonors", convertRepoDonorAggregation(agg));
       } else {
         result.put(name, convertNormalAggregation(agg));
       }
@@ -312,7 +316,25 @@ public class RepositoryFileRepository {
   }
 
   // FIXME: Temporary code
-  private TermFacet convertSumAggregation(Aggregation agg) {
+  // Special aggregation to get unique donor count for each repository
+  private TermFacet convertRepoDonorAggregation(Aggregation agg) {
+    val name = agg.getName();
+    Global globalAgg = (Global) agg;
+    Filter filterAgg = (Filter) globalAgg.getAggregations().get(name);
+    Terms termAgg = (Terms) filterAgg.getAggregations().get(name);
+
+    val termsBuilder = new ImmutableList.Builder<Term>();
+    long total = -1; // Total does not have any meaning in this context because a donor an cross repositories
+    for (val bucket : termAgg.getBuckets()) {
+      val child = (Cardinality) bucket.getAggregations().get("donor");
+      termsBuilder.add(new Term(bucket.getKey(), child.getValue()));
+    }
+    return TermFacet.repoTermFacet(total, 0, termsBuilder.build());
+  }
+
+  // FIXME: Temporary code
+  // Special aggregation to get file size for each repository
+  private TermFacet convertRepoSizeAggregation(Aggregation agg) {
     val name = agg.getName();
     Global globalAgg = (Global) agg;
     Filter filterAgg = (Filter) globalAgg.getAggregations().get(name);
@@ -321,14 +343,10 @@ public class RepositoryFileRepository {
     val termsBuilder = new ImmutableList.Builder<Term>();
     long total = 0;
     for (val bucket : termAgg.getBuckets()) {
-      val key = bucket.getKey();
-      val child = (Sum) bucket.getAggregations().get(name);
-      log.info("children {} {}", key, child.getValue());
-
+      val child = (Sum) bucket.getAggregations().get("fileSize");
       termsBuilder.add(new Term(bucket.getKey(), (long) child.getValue()));
       total += (long) child.getValue();
     }
-
     return TermFacet.repoTermFacet(total, 0, termsBuilder.build());
   }
 
