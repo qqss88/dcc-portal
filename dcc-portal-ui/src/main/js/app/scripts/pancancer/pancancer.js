861dc1dc4a2202c4c637e8f0b69738a7717bf927
mwdchang <mwdchang@gmail.com>
2015-06-22 11:14:20 -0400

    (portal-ui) pancancer landing page stubs

---
+++ dcc-portal-ui/src/main/js/app/scripts/pancancer/pancancer.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  var module = angular.module('icgc.pancancer', ['icgc.pancancer.controllers']);
+
+  module.config(function($stateProvider) {
+    $stateProvider.state('pancancer', {
+      url: '/pcawg',
+      templateUrl: 'scripts/pancancer/views/pancancer.html',
+      controller: 'PancancerController as PancancerController'
+    });
+  });
+
+})();
+
+
+(function() {
+  'use strict';
+
+  var module = angular.module('icgc.pancancer.controllers', []);
+
+  module.controller('PancancerController', function() {
+    // TODO
+  });
+
+})();
