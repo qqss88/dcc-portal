5cc02ccf589acaf15c078cda69dd3732c1a35473
mwdchang <mwdchang@gmail.com>
2015-06-25 09:28:02 -0400

    (portal-ui) pancancer status page stubs

--- dcc-portal-ui/src/main/js/app/scripts/pancancer/pancancer.js
+++ dcc-portal-ui/src/main/js/app/scripts/pancancer/pancancer.js
@@ -1,7 +1,10 @@
 (function() {
   'use strict';
 
-  var module = angular.module('icgc.pancancer', ['icgc.pancancer.controllers']);
+  var module = angular.module('icgc.pancancer', [
+    'icgc.pancancer.controllers',
+    'icgc.pancancer.services'
+  ]);
 
   module.config(function($stateProvider) {
     $stateProvider.state('pancancer', {
@@ -19,9 +22,56 @@
 
   var module = angular.module('icgc.pancancer.controllers', []);
 
-  module.controller('PancancerController', function(Page) {
+  module.controller('PancancerController', function($scope, Page) {
     Page.stopWork();
     Page.setPage('entity');
+
+    function filler(p) {
+      var symbols = ['T','C','G','A', ' '];
+      var result = '';
+
+      for (var i=0; i < p; i++) {
+        for (var ii = 0; ii < Math.random()*1000+500; ii++) {
+          result += symbols[ Math.floor(Math.random()*5)];
+        }
+        result += '. ';
+      }
+      return result;
+    };
+
+    $scope.tcgaIpsum = filler(3);
   });
+
+})();
+
+(function() {
+  'use strict';
+
+  var module = angular.module('icgc.pancancer.services', []);
+
+  module.service('PancancerService', function(Restangular) {
+
+
+    /**
+     * Reorder for UI, the top 5 items are fixed, the remining are appended to the end
+     * on a first-come-first-serve basis.
+     */
+    this.orderPancancerDatatypes = function(data) {
+      var precedence = ['DNA-Seq', 'RNA-Seq', 'SSM', 'CNSM', 'STSM'];
+    };
+
+
+    /**
+     * Get pancancer statistics - This uses ICGC's external repository end point
+     * datatype
+     *   - # donors
+     *   - # files
+     *   - file size
+     */
+    this.getPancancerStats = function() {
+      return Restangular.one('repository/pcawg').get({});
+    };
+  });
+
 
 })();
