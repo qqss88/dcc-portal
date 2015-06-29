cc469d8e1a4700cab2e0b9836cfea74df2fe39a4
mwdchang <mwdchang@gmail.com>
2015-06-29 09:52:37 -0400

    forgot to commit new module

---
+++ dcc-portal-ui/src/main/js/app/scripts/common/pcawg.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  var module = angular.module('icgc.common.pcawg', []);
+
+
+  module.service('PCAWG', function() {
+
+    var data = [
+      {id: 'DNA-Seq', shortLabel: 'Whole Genomes'},
+      {id: 'RNA-Seq', shortLabel: 'Whole Transcriptomes'},
+      {id: 'SSM', shortLabel: 'Simple Somatic Mutataions'},
+      {id: 'CNSM', shortLabel: 'Copy Number Somatic Mutations'},
+      {id: 'StSM', shortLabel: 'Structural Somatic Mutation'}
+    ];
+
+    var shortLabelMap = {};
+    data.forEach(function(datatype) {
+      shortLabelMap[datatype.id] = datatype.shortLabel;
+    });
+
+
+    this.translate = function(id) {
+      return shortLabelMap[id];
+    };
+
+    this.precedence = function() {
+      return _.pluck(data, 'id');
+    };
+
+
+    this.isPCAWGStudy = function(term) {
+      return term === 'PCAWG';
+    };
+
+  });
+
+})();
+
