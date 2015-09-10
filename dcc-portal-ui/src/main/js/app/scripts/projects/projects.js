5a068338fab6766e61ab61f86dc23d589001a938
Douglas Wong <dwong@olm-dwong.local>
2015-09-10 09:30:37 -0400

    Fixed indenting, removed unnecessary empty checker

--- dcc-portal-ui/src/main/js/app/scripts/projects/projects.js
+++ dcc-portal-ui/src/main/js/app/scripts/projects/projects.js
@@ -170,49 +170,58 @@
           data = Restangular.stripRestangular(data);
           _ctrl.distribution = data;
         });
-
-
-
-        if (!_.isEmpty(_.pluck(data.hits, 'id'))){
-          Projects.several(_.pluck(data.hits, 'id').join(',')).get('genes',{
-              include: 'projects',
-              filters: {mutation:{functionalImpact:{is:['High']}}},
-              size: 20
-            }).then(function (genes) {
-              if ( !genes.hits || genes.hits.length === 0) {
-                Page.stopWork();
-                return;
+        if (data.hits == true) {
+          Projects.several(_.pluck(data.hits, 'id').join(',')).get('genes', {
+            include : 'projects',
+            filters : {
+              mutation : {
+                functionalImpact : {
+                  is : [ 'High' ]
+                }
               }
-              var params = {
-                mutation: {functionalImpact:{is:['High']}}
-              };
+            },
+            size : 20
+          }).then(function(genes) {
+            if (!genes.hits || genes.hits.length === 0) {
               Page.stopWork();
-  
-              // FIXME: elasticsearch aggregation support may be more efficient
-              Restangular.one('ui').one('gene-project-donor-counts', _.pluck(genes.hits, 'id'))
-                .get({'filters': params}).then(function(geneProjectFacets) {
-  
-                genes.hits.forEach(function(gene) {
-                  var uiFIProjects = [];
-  
-                  geneProjectFacets[gene.id].terms.forEach(function(t) {
-                    var proj = _.find( data.hits, function(p) {
-                      return p.id === t.term;
-                    });
-  
-                    if (angular.isDefined(proj)) {
-                      uiFIProjects.push({
-                        id: t.term,
-                        name: proj.name,
-                        primarySite: proj.primarySite,
-                        count: t.count
-                      });
-                    }
+              return;
+            }
+            var params = {
+              mutation : {
+                functionalImpact : {
+                  is : [ 'High' ]
+                }
+              }
+            };
+            Page.stopWork();
+
+            // FIXME: elasticsearch aggregation support may be more
+            // efficient
+            Restangular.one('ui').one('gene-project-donor-counts', _.pluck(genes.hits, 'id')).get({
+              'filters' : params
+            }).then(function(geneProjectFacets) {
+
+              genes.hits.forEach(function(gene) {
+                var uiFIProjects = [];
+
+                geneProjectFacets[gene.id].terms.forEach(function(t) {
+                  var proj = _.find(data.hits, function(p) {
+                    return p.id === t.term;
                   });
-                  gene.uiFIProjects = uiFIProjects;
+
+                  if (angular.isDefined(proj)) {
+                    uiFIProjects.push({
+                      id : t.term,
+                      name : proj.name,
+                      primarySite : proj.primarySite,
+                      count : t.count
+                    });
+                  }
                 });
-                _ctrl.stacked = transform(genes.hits);
+                gene.uiFIProjects = uiFIProjects;
               });
+              _ctrl.stacked = transform(genes.hits);
+            });
           });
         } else {
           Page.stopWork();
