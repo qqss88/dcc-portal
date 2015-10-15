17dc9e76e0c2e405fb427aaff66167484d1285fa
Michael Moncada <michael.moncada@oicr.on.ca>
2015-10-15 16:27:44 -0400

    Got JSLint to be happy with the JS in this commit.

--- dcc-portal-ui/src/main/js/app/scripts/repositories/repositories.js
+++ dcc-portal-ui/src/main/js/app/scripts/repositories/repositories.js
@@ -60,13 +60,15 @@
 						},
 						'bodyContent@ICGCcloud.repositories': {
 							templateUrl: function ($stateParams) {
-								return 'scripts/repositories/views/repos/repos.' + _normalizeRepoCode($stateParams.repoCode) + '.content.html';
+								return 	'scripts/repositories/views/repos/repos.' + 
+										_normalizeRepoCode($stateParams.repoCode) + '.content.html';
 							},
 							controller: 'RepositoriesController as repositoryCtrl'
 						},
 						'dataContent@ICGCcloud.repositories': {
 							templateUrl: function ($stateParams) {
-								return 'scripts/repositories/views/repos/repos.' + _normalizeRepoCode($stateParams.repoCode) + '.content.data.html';
+								return 	'scripts/repositories/views/repos/repos.' + 
+										_normalizeRepoCode($stateParams.repoCode) + '.content.data.html';
 							},
 							controller: 'RepositoriesController as repositoryCtrl'
 						}
@@ -77,7 +79,8 @@
 					views: {
 						'repositoryGuides': {
 							templateUrl: function ($stateParams) {
-								return 'scripts/repositories/views/guides/' + _normalizeRepoCode($stateParams.repoCode) + '.html';
+								return 	'scripts/repositories/views/guides/' + 
+										_normalizeRepoCode($stateParams.repoCode) + '.html';
 							},
 							controller: 'RepositoriesGuideController'
 						}
@@ -147,130 +150,8 @@
 	
 	var module = angular.module('icgc.repositories.services', []);
 	
-	module.service('RepositoriesService', function(Restangular, HighchartsService) {
-		function buildRepoFilterStr(datatype) {
-			var filters = {
-				file: {
-				study: {is: ['PCAWG']}
-				}
-			};
+	module.service('RepositoriesService', function(/* Restangular, HighchartsService */) {
 		
-			if (angular.isDefined(datatype)) {
-				filters.file.dataType = {
-				is: [datatype]
-				};
-			}
-		
-			return JSON.stringify(filters);
-		}
-	
-		this.buildRepoFilterStr = buildRepoFilterStr;
-	
-	
-		this.getSiteProjectDonorChart = function(data) {
-		var list = [];
-	
-		// Stack friendly format
-		Object.keys(data).forEach(function(siteKey) {
-			var bar = {};
-			bar.total = 0;
-			bar.stack = [];
-			bar.key = siteKey;
-	
-			Object.keys(data[siteKey]).forEach(function(projKey) {
-			bar.stack.push({
-				name: projKey,
-				label: projKey,
-				count: data[siteKey][projKey],
-				key: siteKey, // parent key
-				colourKey: siteKey,
-				link: '/projects/' + projKey
-			});
-			});
-	
-			bar.stack.sort(function(a, b) { return b.count - a.count; }).forEach(function(p) {
-			p.y0 = bar.total;
-			p.y1 = bar.total + p.count;
-			bar.total += p.count;
-			});
-			list.push(bar);
-		});
-	
-		// Sorted
-		return list.sort(function(a, b) { return b.total - a.total; });
-		};
-	
-	
-		this.getPrimarySiteDonorChart = function(data) {
-		var list = [];
-	
-		Object.keys(data).forEach(function(d) {
-			list.push({
-			id: d,
-			count: data[d],
-			colour: HighchartsService.getPrimarySiteColourForTerm(d)
-			});
-		});
-		list = _.sortBy(list, function(d) { return -d.count; });
-	
-		return HighchartsService.bar({
-			hits: list,
-			xAxis: 'id',
-			yValue: 'count'
-		});
-		};
-	
-	
-		/**
-		* Reorder for UI, the top 5 items are fixed, the remining are appended to the end
-		* on a first-come-first-serve basis.
-		*/
-		this.orderDatatypes = function(data) {
-		var precedence = PCAWG.precedence();
-		var list = [];
-	
-	
-		// Scrub
-		data = Restangular.stripRestangular(data);
-	
-		// Flatten and normalize for display
-		Object.keys(data).forEach(function(key) {
-			list.push({
-			name: key,
-			uiName: PCAWG.translate(key),
-			donorCount: +data[key].donorCount,
-			fileCount: +data[key].fileCount,
-			fileSize: +data[key].fileSize,
-			fileFormat: data[key].dataFormat,
-			filters: buildRepoFilterStr(key)
-			});
-		});
-	
-		// Sort
-		return _.sortBy(list, function(d) {
-			return precedence.indexOf(d.name);
-		});
-	
-		};
-	
-	
-		/**
-		* Get pancancer statistics - This uses ICGC's external repository end point
-		* datatype
-		*   - # donors
-		*   - # files
-		*   - file size
-		*/
-		this.getPancancerStats = function() {
-		return Restangular.one('repository/files/pcawg/stats').get({});
-		};
-	
-		this.getPancancerSummary = function() {
-		var param = {
-			filters: {file: { study: {is: ['PCAWG']}}}
-		};
-		return Restangular.one('repository/files/summary').get(param);
-		};
-  });
+  	});
 	
 })();
